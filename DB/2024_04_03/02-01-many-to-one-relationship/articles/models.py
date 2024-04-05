2ad3113671baaf40c17a9125b640eedeb9c8cc1d
solmysoul <solmysoul1@gmail.com>
2024-04-05 17:47:58 +0900

    add

---
+++ DB/2024_04_03/02-01-many-to-one-relationship/articles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    # 연결/ 게시글이 지워지면 관련된 것도 지움
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ DB/2024_04_04/02-02-many-to-one-relationship/articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+# 장고는 User 모델을 직접 참조하지 않음
+# User 모델을 직접 참조했을 경우 이름이 바뀌거나 대체 됐을 때 유지보수가 어려움
+from django.conf import settings
+
+# Create your models here.
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
